13fab6b16251eb5798668beefa70da86743f3626
solji <thfwl0094@naver.com>
2024-03-06 14:19:53 +0900

    version 0.0.7

--- src/Test2.js
+++ src/Test2.js
@@ -4,7 +4,7 @@ const Test = () => {
     return React.createElement(
         'div',
         null,
-        React.createElement('h1', null, '부모 패키지야22222')
+        React.createElement('h1', null, '부모 패키지야ABCD')
     );
 };
 
